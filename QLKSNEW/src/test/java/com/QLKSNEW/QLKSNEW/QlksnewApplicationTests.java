489cdd6735c33b8157ee4f86aa8a71650d24c5c0
Cao Tien Hoang <79745516+maxius201@users.noreply.github.com>
2023-03-19 12:42:25 +0700

    Add files via upload

---
+++ QLKSNEW/src/test/java/com/QLKSNEW/QLKSNEW/QlksnewApplicationTests.java
@@ -0,0 +1,13 @@
+package com.QLKSNEW.QLKSNEW;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QlksnewApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
